dac2ec830c924e2d0310aba961e7faa29b900b79
Gunho Park <devgun@kakao.com>
2023-07-02 15:42:09 +0900

    feat: add logger

--- demo/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ demo/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,11 +1,20 @@
 package com.example.demo
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class DemoApplication
+class DemoApplication {
+    companion object {
+        private val logger: Logger = LoggerFactory.getLogger(this::class.java)
 
-fun main(args: Array<String>) {
-	runApplication<DemoApplication>(*args)
+        @JvmStatic
+        fun main(args: Array<String>) {
+            logger.info("Application starting...")
+            runApplication<DemoApplication>(*args)
+            logger.info("Application started.")
+        }
+    }
 }
